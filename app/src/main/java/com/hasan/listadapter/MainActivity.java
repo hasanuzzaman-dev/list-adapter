c92318d150105abeb3cebbf2127acf0616d581a4
Hasan <hasanuzzaman.dev@gmail.com>
2020-11-24 18:32:53 +0600

    set recyclerview

--- app/src/main/java/com/hasan/listadapter/MainActivity.java
+++ app/src/main/java/com/hasan/listadapter/MainActivity.java
@@ -1,21 +1,42 @@
 package com.hasan.listadapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.hasan.listadapter.adapters.MovieListAdapter;
+import com.hasan.listadapter.models.Movie;
+
+
+public class MainActivity extends AppCompatActivity implements MovieListAdapter.MovieClickInterface {
+
+    private MovieListAdapter movieListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        movieListAdapter = new MovieListAdapter(Movie.itemCallback,this);
+        recyclerView.setAdapter(movieListAdapter);
     }
 
     public void addItem(View view) {
     }
 
     public void updateItem(View view) {
+    }
+
+
+    @Override
+    public void onDelete(int position) {
+
     }
 }
---
+++ app/src/main/java/com/hasan/listadapter/adapters/MovieListAdapter.java
@@ -0,0 +1,67 @@
+package com.hasan.listadapter.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hasan.listadapter.R;
+import com.hasan.listadapter.models.Movie;
+
+public class MovieListAdapter extends ListAdapter<Movie, MovieListAdapter.MovieViewHolder> {
+
+    MovieClickInterface movieClickInterface;
+
+    public MovieListAdapter(@NonNull DiffUtil.ItemCallback<Movie> diffCallback, MovieClickInterface movieClickInterface) {
+        super(diffCallback);
+        this.movieClickInterface = movieClickInterface;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_row,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+
+        Movie movie = getItem(position);
+        holder.nameTV.setText(movie.getName());
+        holder.ratingTV.setText(movie.getRating());
+
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTV, ratingTV;
+        Button deleteBtn;
+
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameTV = itemView.findViewById(R.id.nameTextview);
+            ratingTV = itemView.findViewById(R.id.ratingTextView);
+            deleteBtn = itemView.findViewById(R.id.deleteButton);
+
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    movieClickInterface.onDelete(getAdapterPosition());
+
+                }
+            });
+        }
+    }
+
+    public interface MovieClickInterface{
+        void onDelete(int position);
+    }
+}
